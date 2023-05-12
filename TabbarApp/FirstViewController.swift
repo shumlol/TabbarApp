35149d209c344bdfa9df45d1ed04f81aeba9507e
Vadim Akhma <shummmmmm@gmail.com>
2023-05-12 19:16:26 +0600

    add new VC

---
+++ TabbarApp/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  TabbarApp
+//
+//  Created by Вадим Ахматханов on 12.05.2023.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
